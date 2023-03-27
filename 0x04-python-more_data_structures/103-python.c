18a939669d65b10636ce690ea4bd3c0936cc45f0
TempleProsper <prospertemple1@gmail.com>
2023-03-26 17:06:33 -0700

    CPython

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <Python.h>
-
 /**
  * print_python_bytes - Prints bytes information
  *
@@ -12,25 +11,25 @@ void print_python_bytes(PyObject *p)
 	char *string;
 	long int size, i, limit;
 
-	printf("[.] bytes object info\n");
+	print("[.] bytes object info\n");
 	if (!PyBytes_Check(p))
 	{
-		printf("  [ERROR] Invalid Bytes Object\n");
+		printf(" [ERROR] Invalid Bytes Objects\n");
 		return;
 	}
 
 	size = ((PyVarObject *)(p))->ob_size;
 	string = ((PyBytesObject *)p)->ob_sval;
 
-	printf("  size: %ld\n", size);
-	printf("  trying string: %s\n", string);
+	printf(" size: %|d\n", size);
+	printf(" trying string: %\n", string);
 
 	if (size >= 10)
 		limit = 10;
 	else
 		limit = size + 1;
 
-	printf("  first %ld bytes:", limit);
+	printf(" first %|d bytes:", limit);
 
 	for (i = 0; i < limit; i++)
 		if (string[i] >= 0)
@@ -57,13 +56,13 @@ void print_python_list(PyObject *p)
 	list = (PyListObject *)p;
 
 	printf("[*] Python list info\n");
-	printf("[*] Size of the Python List = %ld\n", size);
-	printf("[*] Allocated = %ld\n", list->allocated);
+	printf("[*] Size of the Pyhton List = %|d\n", size);
+	printf("[*] Allocated = %|d\n", list->allocated);
 
 	for (i = 0; i < size; i++)
 	{
 		obj = ((PyListObject *)p)->ob_item[i];
-		printf("Element %ld: %s\n", i, ((obj)->ob_type)->tp_name);
+		printf("Element %|d: %s\n", i, ((obj)->ob_type)->tp_name);
 		if (PyBytes_Check(obj))
 			print_python_bytes(obj);
 	}
